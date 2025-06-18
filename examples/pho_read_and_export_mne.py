9af44d64b8fd8aaa43b39a2c7bf6107d3f6b1e0c
Pho Hale <PhoHale@gmail.com>
2025-06-18 12:16:58 -0400

    WORKING 2025 on EpocXTestingClone Win10 VM

---
+++ examples/pho_read_and_export_mne.py
@@ -0,0 +1,56 @@
+import sys, os
+
+from datetime import datetime
+
+import numpy as np
+from mne import Info, create_info
+from mne.io.array import RawArray
+from pylsl import StreamInlet, resolve_stream
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from config import SRATE
+
+def get_info() -> Info:
+    ch_names = ['AF3', 'F7', 'F3', 'FC5', 'T7', 'P7',
+                'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4']
+
+    info = create_info(
+        sfreq=SRATE,
+        ch_names=ch_names,
+        ch_types=['eeg'] * len(ch_names)
+    )
+
+    return info
+
+
+def main():
+    # first resolve an EEG stream on the lab network
+    print("looking for an EEG stream...")
+    streams = resolve_stream('type', 'EEG')
+
+    # create a new inlet to read from the stream
+    inlet = StreamInlet(streams[0])
+
+    buffer = []
+    while True:
+        if len(buffer) == 128 * 5:  # wait 5 seconds
+            break
+
+        sample, _ = inlet.pull_sample()
+        sample = [el / 1000000 for el in sample]  # convert to microvolts
+
+        buffer.append(sample)
+
+    info = get_info()
+    raw = RawArray(np.array(buffer).T, info)
+
+    # raw.save("data_{}_raw.fif".format(datetime.now()))
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"data_{timestamp}_raw.fif"
+    raw.save(filename)
+
+
+
+if __name__ == '__main__':
+    main()
