3ad5bdd3dca9af5289df717ac22bcfa7eec3a490
Pho Hale <PhoHale@gmail.com>
2025-07-02 07:13:05 -0400

    windows scripts improvements

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from emotiv_lsl.emotiv_epoc_x import EmotivEpocX
 
 if __name__ == "__main__":
     emotiv_epoc_x = EmotivEpocX()
+    crypto_key = emotiv_epoc_x.get_crypto_key()
+    print(f'crypto_key: {crypto_key}')
     emotiv_epoc_x.main_loop()
