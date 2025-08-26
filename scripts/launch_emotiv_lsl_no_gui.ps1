2e870bc3569b41318b24c1b4bff28eed6814a396
Pho Hale <PhoHale@gmail.com>
2025-08-26 12:00:16 -0400

    bump

--- scripts/launch_emotiv_lsl_no_gui.ps1
+++ scripts/launch_emotiv_lsl_no_gui.ps1
@@ -33,7 +33,7 @@ Start-Sleep -Seconds 5
 
 # Launch the viewer component
 Write-Host "Starting BSL Recorders..." -ForegroundColor Cyan
-Start-CommandWindow -Title "BSL EEG Recorder" -Command "cd '$repoRoot'; micromamba activate lsl_env; bsl_stream_recorder --stream_name 'Epoc X' --record_dir '\\vmware-host\Shared Folders\Emotiv Epoc EEG Project\EEG Recordings - Any\EEG Recordings - Dropbox\EmotivEpocX_EEGRecordings';"
-Start-CommandWindow -Title "BSL MotionRecorder" -Command "cd '$repoRoot'; micromamba activate lsl_env; bsl_stream_recorder --stream_name 'Epoc X Motion' --record_dir '//vmware-host/Shared Folders/Emotiv Epoc EEG Project/EEG Recordings - Any/EEG Recordings - Dropbox/EmotivEpocX_EEGRecordings/MOTION_RECORDINGS';"
+Start-CommandWindow -Title "BSL EEG Recorder" -Command "cd '$repoRoot'; micromamba activate lsl_env; bsl_stream_recorder --stream_name 'Epoc X' -d '\\vmware-host\Shared Folders\Emotiv Epoc EEG Project\EEG Recordings - Any\EEG Recordings - Dropbox\EmotivEpocX_EEGRecordings';"
+Start-CommandWindow -Title "BSL MotionRecorder" -Command "cd '$repoRoot'; micromamba activate lsl_env; bsl_stream_recorder --stream_name 'Epoc X Motion' -d '//vmware-host/Shared Folders/Emotiv Epoc EEG Project/EEG Recordings - Any/EEG Recordings - Dropbox/EmotivEpocX_EEGRecordings/MOTION_RECORDINGS';"
 
 Write-Host "All components launched successfully!" -ForegroundColor Green
