2bde9aa538407e8193f6d9307b235cdade8e6cc7
Pho Hale <CommanderPho@users.noreply.github.com>
2025-11-04 19:44:08 -0500

    fix macOS script

--- scripts/build_macOS_app.py
+++ scripts/build_macOS_app.py
@@ -2,30 +2,37 @@ import PyInstaller.__main__
 import sys
 from pathlib import Path
 
-# Get the directory where this script is located
-script_dir = Path(__file__).parent
-main_app_dir = script_dir.parent.resolve()
-print(f'main_app_dir: "{main_app_dir.as_posix()}"')
+def main() -> None:
+    # Get the directory where this script is located
+    script_dir = Path(__file__).parent
+    repo_root = script_dir.parent.resolve()
+    print(f'repo_root: "{repo_root.as_posix()}"')
+    spec_file = repo_root / "main.spec"
 
-# Run PyInstaller with the right options
-PyInstaller.__main__.run([
-    'logger_app.py',
-    '--onedir',  # Create a single executable file
-    '--windowed',  # Hide console window (for GUI apps)
-    '--name=PhoLogToLabStreamingLayer',
-    '--icon=icons/LogToLabStreamingLayerIcon_Light.ico',  # Optional: add an icon file
-    # '--add-data=logger_app.py;.',  # Include all Python files
-    '--hidden-import=pylsl',
-    '--hidden-import=mne',
-    '--hidden-import=numpy',
-    '--hidden-import=tkinter',
-    '--hidden-import=threading',
-    '--hidden-import=json',
-    '--hidden-import=pathlib',
-    '--hidden-import=datetime',
-    '--collect-all=mne',  # Include all MNE data files
-    '--collect-all=pylsl',  # Include all PyLSL data files
-    f'--distpath={main_app_dir}/dist',
-    f'--workpath={main_app_dir}/build',
-    f'--specpath={main_app_dir}',
-])
+
+    if spec_file.exists():
+        # Run PyInstaller with the right options
+        PyInstaller.__main__.run([
+            'main.py',
+            '--onedir',  # Create a single executable file
+            '--windowed',  # Hide console window (for GUI apps)
+            "--name=emotiv_lsl",
+            "--icon=images/icons/emotiv_lsl_icon_design.ico",  # Optional: add an icon file
+            "--add-data=*.py;.",  # Include all Python files
+            '--hidden-import=pylsl',
+            '--hidden-import=mne',
+            '--collect-all=mne',  # Include all MNE data files
+            '--collect-all=pylsl',  # Include all PyLSL data files
+            f'--distpath={repo_root}/dist',
+            f'--workpath={repo_root}/build',
+            f'--specpath={repo_root}',
+            "--noconfirm",
+            "--clean",
+        ])
+
+
+
+if __name__ == "__main__":
+    main()
+
+
